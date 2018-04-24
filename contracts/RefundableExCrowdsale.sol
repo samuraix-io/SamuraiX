d2d965f0cbbd441727f0d20ed61d94b9ca64a23e
tinhnt <tinh.nt@anlab.info>
2018-04-24 10:56:08 +0700

    Add comments for RefundableExCrowdsale.sol

--- contracts/RefundableExCrowdsale.sol
+++ contracts/RefundableExCrowdsale.sol
@@ -5,18 +5,19 @@ import "./RefundableExVault.sol";
 
 
 /*
- * @title RefundableExCrowdsale
- * @dev Extension of RefundableCrowdsale contract that adds a funding goal, and
- * the possibility of users getting a refund if goal is not met.
- * Uses a RefundVault as the crowdsale's vault.
+ * @title Refundable extended crowdsale.
+ * @dev Extension of RefundableCrowdsale contract that adds
+ * the possibility of using RAX tokens.
+ * Uses a RefundableExVault as the crowdsale's vault.
  */
 contract RefundableExCrowdsale is RefundableCrowdsale {
   // refund vault used to hold funds while crowdsale is running
   RefundableExVault public exVault;
 
   /*
-   * @dev Constructor, creates RefundVault.
-   * @param _goal Funding goal
+   * @dev Constructor, creates RefundableExVault.
+   * @param _token Address of RAX token contract.
+   * @param _goal Funding goal.
    */
   function RefundableExCrowdsale(RAXToken _token, uint256 _goal)
   RefundableCrowdsale(_goal)
@@ -24,14 +25,26 @@ contract RefundableExCrowdsale is RefundableCrowdsale {
     exVault = new RefundableExVault(_token, wallet);
   }
 
+  /*
+   * @dev Gets address of the vault.
+   * @return Vault address.
+   */
   function getVaultAddress() public view returns(address) {
     return address(exVault);
   }
 
+  /*
+   * @dev Forwards RAX tokens from the investor to the vault.
+   * @param _investor Investor address.
+   * @param _amount Amount of tokens to forward.
+   */
   function _forwardFundsRAX(uint256 _amount) internal {
     exVault.depositRAX(msg.sender, _amount);
   }
 
+  /*
+   * @dev Forwards Ether from the investor to the vault.
+   */
   function _forwardFundsEther() internal {
     exVault.deposit.value(msg.value)(msg.sender);
   }
