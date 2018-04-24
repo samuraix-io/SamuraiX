e61e6a9671d826f0d84b90b30efad75956b902c1
tinhnt <tinh.nt@anlab.info>
2018-04-24 10:55:25 +0700

    Add comments for RefundableExVault.sol

--- contracts/RefundableExVault.sol
+++ contracts/RefundableExVault.sol
@@ -4,23 +4,30 @@ pragma solidity ^0.4.20;
 import "zeppelin-solidity/contracts/crowdsale/distribution/utils/RefundVault.sol";
 import "./RAXToken.sol";
 
-
+/**
+ * @title Refundable extended vault.
+ * @dev RefundVault added with supporting RAX tokens in action.
+ * This contract is used for storing funds while a crowdsale
+ * is in progress. Supports refunding the money if crowdsale fails,
+ * and forwarding it if crowdsale is successful.
+ */
 contract RefundableExVault is RefundVault {
   mapping (address => uint256) public depositedRAX;
 
   RAXToken raxToken;
 
   /*
-   * @param _token RAXToken
-   * @param _wallet Vault address
+   * @param _token Address of RAX token contract.
+   * @param _wallet Address for forwarding the money if the crowdsale is successful.
    */
   function RefundableExVault(RAXToken _token, address _wallet)
   RefundVault(_wallet) {
     raxToken = _token;
   }
 
-  /*
-   * @param investor Investor address
+  /**
+   * @dev Stores RAX tokens while the crowdsale is in progress.
+   * @param _investor Investor address.
    */
   function depositRAX(address _sender, uint256 _amount) onlyOwner public {
     require(state == State.Active);
@@ -29,6 +36,9 @@ contract RefundableExVault is RefundVault {
     raxToken.transferFrom(_sender, this, _amount);
   }
 
+  /**
+   * @dev Closes this vault and forwards the money if crowdsale is successful.
+   */
   function close() onlyOwner public {
     super.close();
 
@@ -36,8 +46,9 @@ contract RefundableExVault is RefundVault {
     raxToken.transfer(wallet, _amount);
   }
 
-  /*
-   * @param investor Investor address
+  /**
+   * @dev Refunds the money if crowdsale fails.
+   * @param _investor Investor address.
    */
   function refund(address _investor) public {
     super.refund(_investor);
