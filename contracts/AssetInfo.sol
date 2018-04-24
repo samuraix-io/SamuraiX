155d15c4c1754c4f391f1d1a4e6d0e8b62141425
tinhnt <tinh.nt@anlab.info>
2018-04-24 10:42:33 +0700

    Add comments for AssetInfo.sol

--- contracts/AssetInfo.sol
+++ contracts/AssetInfo.sol
@@ -2,15 +2,31 @@ pragma solidity ^0.4.20;
 
 import "./Manageable.sol";
 
-
+/**
+ * @title Asset information.
+ * @dev Stores information about a specified real asset.
+ */
 contract AssetInfo is Manageable {
   string fixedDocsLink;
   string fixedDocsHash;
   string varDocsLink;
   string varDocsHash;
 
+  /**
+   * Event for updated running documents logging.
+   * @param _token Token associated with this asset.
+   * @param _linkDoc A new link of the newly updated file.
+   * @param _hashDoc A new hash value corresponding to the newly updated file.
+   */
   event UpdateRunningDocuments(address _token, string _linkDoc, string _hashDoc);
 
+  /**
+   * @param _managers Managers of this asset.
+   * @param _fixedDocsLink A link to a zip file containing fixed legal documents of the asset.
+   * @param _fixedDocsHash Hash value of the zip file containing fixed legal documents of the asset.
+   * @param _varDocsLink A link to a zip file containing running documents of the asset.
+   * @param _varDocsHash Hash value of the zip file containing running documents of the asset.
+   */
   function AssetInfo (
     address[] _managers,
     string _fixedDocsLink,
@@ -26,10 +42,20 @@ contract AssetInfo is Manageable {
     varDocsHash = _varDocsHash;
   }
 
+  /**
+   * @dev Gets information about where to find fixed legal documents of this asset.
+   * @return _link A link to a zip file containing fixed legal documents of the asset.
+   * @return _hash Hash value of the zip file.
+   */
   function getFixedDocuments() public view returns(string _link, string _hash) {
     return(fixedDocsLink, fixedDocsHash);
   }
 
+  /**
+   * @dev Updates information about where to find new running documents of this asset.
+   * @param _link A link to a zip file containing running documents of the asset.
+   * @param _hash Hash value of the zip file.
+   */
   function changeRunningDocuments(string _link, string _hash) public onlyManager {
     varDocsLink = _link;
     varDocsHash = _hash;
@@ -37,6 +63,11 @@ contract AssetInfo is Manageable {
     emit UpdateRunningDocuments(this, _link, _hash);
   }
 
+  /**
+   * @dev Gets information about where to find running documents of this asset.
+   * @return _link A link to a zip file containing running documents of the asset.
+   * @return _hash Hash value of the zip file.
+   */
   function getRunningDocuments() public view returns(string _link, string _hash) {
     return(varDocsLink, varDocsHash);
   }
