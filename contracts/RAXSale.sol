2e9d18ab4e797f4cdb825e5b0ed70c432de7443a
tinhnt <tinh.nt@anlab.info>
2018-04-27 09:46:22 +0700

    Clean code

--- contracts/RAXSale.sol
+++ contracts/RAXSale.sol
@@ -5,18 +5,18 @@ import './RAXToken.sol';
 
 
 /*
- * PreSale is
+ * RAXSale is
  *  - capped at 2 billion tokens
  *  - 0.1 ether minimum purchase amount
  *  - exchange rate varies with purchase amount
  */
-contract PreSale is RAXCrowdsale {
+contract RAXSale is RAXCrowdsale {
     using SafeMath for uint256;
 
     uint256 public constant PRESALE_TOKEN_CAP = 2 * (10**9) * (10 ** uint256(18)); // 2 billion tokens
     uint256 public minPurchaseAmt = 100 finney;
 
-    function PreSale(MintableToken _token, uint256 _startTime, uint256 _endTime, address _ethWallet, uint256 _rate)
+    function RAXSale(MintableToken _token, uint256 _startTime, uint256 _endTime, address _ethWallet, uint256 _rate)
     RAXCrowdsale(_token, _startTime, _endTime, _ethWallet, _rate)
     {
     }
