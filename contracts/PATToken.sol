5d0057ce495cebfcefb01e51504d661eb4a31dc0
tinhnt <tinh.nt@anlab.info>
2018-04-27 19:03:11 +0700

    Add comments for PATToken.sol

--- contracts/PATToken.sol
+++ contracts/PATToken.sol
@@ -7,15 +7,19 @@ import 'zeppelin-solidity/contracts/ownership/Contactable.sol';
 import './ManageableToken.sol';
 
 
-/*
- * PATToken is a ERC20 token that
- *  - caps total number at 100 million tokens
- *  - can pause and unpause token transfer (and authorization) actions
- *  - mints new tokens when purchased (rather than transferring tokens pre-granted to a holding account)
- *  - attempts to reject ERC20 token transfers to itself and allows token transfer out
- *  - attempts to reject ether sent and allows any ether held to be transferred out
- */
- // Destructible
+/**
+ * @title PAT token.
+ * @dev PAT is a ERC20 token that:
+ *  - caps total number at 100 million tokens.
+ *  - can pause and unpause token transfer (and authorization) actions.
+ *  - can be disabled/enabled.
+ *  - mints new tokens when purchased.
+ *  - token holders can be distributed profit from asset manager.
+ *  - contains real asset information.
+ *  - attempts to reject token transfer to any unregistered users.
+ *  - attempts to reject ERC20 token transfers to itself and allows token transfer out.
+ *  - attempts to reject ether sent and allows any ether held to be transferred out.
+ **/
 contract PATToken is Contactable, HasNoTokens, HasNoEther, PausableToken, ManageableToken {
   string public name;
   string public symbol;
@@ -25,6 +29,17 @@ contract PATToken is Contactable, HasNoTokens, HasNoEther, PausableToken, Manage
   uint256 public constant MILLION_TOKENS = (10**6) * ONE_TOKENS;
   uint256 public constant TOTAL_TOKENS = 100 * MILLION_TOKENS;
 
+  /**
+   * @param _regUsers A contract to check whether an account is registered or not.
+   * @param _id Unique ID of this token. _id must be greater than 1.
+   * @param _managers Managers of a real asset associated with this token.
+   * @param _name Name of this token.
+   * @param _symbol Symbol of this token.
+   * @param _fixedDocsLink A link to a zip file containing fixed legal documents of the asset.
+   * @param _fixedDocsHash Hash value of the zip file containing fixed legal documents of the asset.
+   * @param _varDocsLink A link to a zip file containing running documents of the asset.
+   * @param _varDocsHash Hash value of the zip file containing running documents of the asset.
+   */
   function PATToken(
     RegisteredUsers _regUsers,
     uint256 _id,
@@ -52,30 +67,41 @@ contract PATToken is Contactable, HasNoTokens, HasNoEther, PausableToken, Manage
     contactInformation = 'https://token.samuraix.io/';
   }
 
-  // cap minting so that totalSupply <= TOTAL_TOKENS
-  function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
+  /**
+   * @dev Mints tokens to a beneficiary address.
+   * Cap minting so that totalSupply <= TOTAL_TOKENS.
+   * @param _to Who got the tokens.
+   * @param _amount Amount of tokens.
+   */
+  function mint(address _to, uint256 _amount) onlyOwner canMint public returns(bool) {
     require(totalSupply_.add(_amount) <= TOTAL_TOKENS);
     return super.mint(_to, _amount);
   }
 
-  /*
-  * @dev Allows the current owner to transfer control of the contract to a newOwner.
-  * @param newOwner The address to transfer ownership to.
-  */
-  function transferOwnership(address newOwner) onlyOwner public {
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a new owner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address _newOwner) onlyOwner public {
     // do not allow self ownership
-    require(newOwner != address(this));
-    super.transferOwnership(newOwner);
+    require(_newOwner != address(this));
+    super.transferOwnership(_newOwner);
   }
 
+  /**
+   * @dev Gets total tokens.
+   * @return Total tokens.
+   */
   function getTotalTokens() public view returns(uint256) {
       return TOTAL_TOKENS;
   }
 
-  function isManager(address _addr) public view returns(bool) {
-    return super.isManager(_addr);
-  }
-
+  /**
+   * @dev Calculates profit to distribute to a specified token holder.
+   * @param _totalProfit Total profit.
+   * @param _holder Token holder address.
+   * @return Profit value relevant to the token holder.
+   */
   function calculateProfit(uint256 _totalProfit, address _holder) public view returns(uint256) {
     require(_totalProfit > 0);
     require(isHolder(_holder));
