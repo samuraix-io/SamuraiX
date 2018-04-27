3f23da89effc16487cd0c46eecf0d61b62602350
tinhnt <tinh.nt@anlab.info>
2018-04-27 17:27:24 +0700

    Add comments for PATCrowdsaleBase.sol

--- contracts/PATCrowdsaleBase.sol
+++ contracts/PATCrowdsaleBase.sol
@@ -11,16 +11,19 @@ import './RefundableExCrowdsale.sol';
 
 
 /*
- * PATCrowdsale is the base class for both the PreSale and MainSale.
+ * PATCrowdsaleBase is the base class for both the PATCrowdsaleEther and PATCrowdsaleRAX.
  * It is a Crowdsale that is:
- *  - time capped by start and end dates
- *  - value capped by number of tokens sold (and not ether raised)
- *  - supports a variable exchange rate by allowing sub-classes to override applyExchangeRate()
- *  - pause() and unpause() to control token purchases
- *  - finalize() transfers token ownership to this.owner
- *  - attempts to reject ERC20 token transfers to itself and allows token transfer out
- *  - allows child contract ownership to be transferred to this.owner
- *  - allows wallet which receives sales proceeds to be updated
+ *  - time capped by start and end dates.
+ *  - value capped by number of tokens sold (and not ether raised).
+ *  - supports a variable exchange rate by allowing sub-classes to override _weiToPAT() and _raxToWei().
+ *  - pause() and unpause() to control token purchases.
+ *  - finalize() transfers token ownership to this.owner.
+ *  - accepts RAX token transfers to itself and allows token transfer out.
+ *  - allows child contract ownership to be transferred to this.owner.
+ *  - allows wallet which receives sales proceeds to be updated.
+ *  - allows end date to be updated.
+ *  - any unregistered users can not buy tokens.
+ *  - refundable.
  */
 contract PATCrowdsaleBase is Contactable, Pausable, HasNoContracts, RefundableExCrowdsale {
   using SafeMath for uint256;
@@ -29,7 +32,15 @@ contract PATCrowdsaleBase is Contactable, Pausable, HasNoContracts, RefundableEx
   uint256 public tokensSold = 0;
   RegisteredUsers public regUsers;
 
-  // ignore the Crowdsale.rate and dynamically compute rate based on other factors (e.g. purchase amount, time, etc)
+  /**
+   * @param _regUsers A contract to check whether an account is registered or not.
+   * @param _raxToken RAX token contract address.
+   * @param _token PAT token contract address.
+   * @param _startTime Start date.
+   * @param _endTime End data.
+   * @param _ethWallet A wallet address to receive sales proceeds if crowdsale is successful.
+   * @param _weiAmountGoal Minimum wei amount to raise.
+   */
   function PATCrowdsaleBase(
     RegisteredUsers _regUsers,
     RAXToken _raxToken,
@@ -52,6 +63,10 @@ contract PATCrowdsaleBase is Contactable, Pausable, HasNoContracts, RefundableEx
     contactInformation = 'https://token.samuraix.io/';
   }
 
+  /**
+   * @dev Changes a wallet address to receive sales proceeds if crowdsale is successful.
+   * @param _wallet A new wallet address.
+   */
   function setWallet(address _wallet) external {
     require(_wallet != 0x0);
     require(getTokenContract().isManager(msg.sender));
@@ -59,6 +74,10 @@ contract PATCrowdsaleBase is Contactable, Pausable, HasNoContracts, RefundableEx
     wallet = _wallet;
   }
 
+  /**
+   * @dev Changes end date of the crowdsale.
+   * @param _endTime A new end date.
+   */
   function setEndTime(uint256 _endTime) external {
     require(_endTime > block.timestamp);
     require(getTokenContract().isManager(msg.sender));
@@ -66,6 +85,12 @@ contract PATCrowdsaleBase is Contactable, Pausable, HasNoContracts, RefundableEx
     closingTime = _endTime;
   }
 
+  /**
+   * @dev Mints tokens to a buyer and adds him to the token holders list.
+   * @param _beneficiary Who got the tokens.
+   * @param _weiAmount Wei amount to exchange.
+   * @return The number of minted tokens.
+   */
   function _buyTokens(address _beneficiary, uint256 _weiAmount) internal returns(uint256) {
     require(_beneficiary != 0x0);
     require(_weiAmount > 0);
@@ -86,30 +111,41 @@ contract PATCrowdsaleBase is Contactable, Pausable, HasNoContracts, RefundableEx
     return _tokens;
   }
 
-  // overriding Crowdsale#hasEnded to add cap logic
-  // @return true if crowdsale event has ended
+  /**
+   * @dev Checks whether the crowdsale has ended or not.
+   * Overriding Crowdsale.hasEnded to add cap logic.
+   * @return True if crowdsale event has ended, otherwise false.
+   */
   function hasEnded() public view returns(bool) {
     bool capReached = tokensRemaining() == 0;
     return super.hasClosed() || capReached;
   }
 
-  function tokenTransferOwnership(address newOwner) public onlyOwner {
+  /**
+   * @dev Transfers ownership of the token when the crowdsale has ended.
+   * @param _newOwner New owner address.
+   */
+  function tokenTransferOwnership(address _newOwner) public onlyOwner {
     require(hasEnded() == true);
-    Ownable(token).transferOwnership(newOwner);
+    Ownable(token).transferOwnership(_newOwner);
   }
 
   /*
-  * @dev Allows the current owner to transfer control of the contract to a newOwner.
-  * @param newOwner The address to transfer ownership to.
+  * @dev Allows the current owner to transfer control of the contract to a new owner.
+  * @param _newOwner The address to transfer ownership to.
   */
-  function transferOwnership(address newOwner) onlyOwner public {
+  function transferOwnership(address _newOwner) onlyOwner public {
     // do not allow self ownership
-    require(newOwner != address(this));
-    super.transferOwnership(newOwner);
+    require(_newOwner != address(this));
+    super.transferOwnership(_newOwner);
   }
 
-  // sub-classes must override to control tokens sales cap
-  function tokensRemaining() view public returns (uint256);
+  /**
+   * @dev Gets the number of remaining tokens.
+   * Sub-classes must override to control tokens sales cap.
+   * @return The number of remaining tokens.
+   */
+  function tokensRemaining() view public returns(uint256);
 
   /*
    * internal functions
@@ -128,12 +164,27 @@ contract PATCrowdsaleBase is Contactable, Pausable, HasNoContracts, RefundableEx
     super.finalization();
   }
 
+  /**
+   * @dev Gets the token contract.
+   * @return The token contract.
+   */
   function getTokenContract() internal view returns(PATToken) {
     return PATToken(token);
   }
 
-  // sub-classes must override to customize token-per-wei exchange rate
+  /**
+  * @dev Exchanges from Wei to PAT token.
+  * Sub-classes must override to customize exchange rate.
+  * @param _wei Amount of Wei to exchange.
+  * @return The number of PAT token units exchanged.
+  */
   function _weiToPAT(uint256 _wei) view internal returns(uint256);
 
+  /**
+  * @dev Exchanges from RAT token to Wei.
+  * Sub-classes must override to customize exchange rate.
+  * @param _amount Amount of RAT token units.
+  * @return Amount of Wei exchanged.
+  */
   function _raxToWei(uint256 _amount) view internal returns(uint256);
 }
