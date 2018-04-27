ced7d5b593c6c51e7501ffac9bf5422df9d8cd59
tinhnt <tinh.nt@anlab.info>
2018-04-27 19:23:01 +0700

    Add comments for TokenHolders.sol

--- contracts/TokenHolders.sol
+++ contracts/TokenHolders.sol
@@ -2,11 +2,20 @@ pragma solidity ^0.4.20;
 
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
-
+/**
+ * @title Token holders.
+ * @dev This contract allows a sub-class token contract to store
+ * and run a loop through its all holders.
+ **/
 contract TokenHolders is Ownable {
   mapping(address => bool) holderCheck;
   address[] holders;
 
+  /**
+   * @dev Adds a new account to the token holders list.
+   * @param _holder Holder address.
+   * @return True if succeed, otherwise false.
+   */
   function addHolder(address _holder) public returns(bool) {
     require(msg.sender == address(this) || msg.sender == owner);
 
@@ -19,16 +28,30 @@ contract TokenHolders is Ownable {
     return true;
   }
 
+  /**
+   * @dev Checks whether an account is in the token holders list or not.
+   * @param _addr Address to check.
+   * @return True if the account is a token holder, otherwise false.
+   */
   function isHolder(address _addr) public view returns(bool) {
     return holderCheck[_addr];
   }
 
+  /**
+   * @dev Gets a holder address at a specified index in the holders list.
+   * @param _index Index.
+   * @return A relevant holder address.
+   */
   function getHolderAddress(uint256 _index) public view returns(address) {
     require(_index < holders.length);
 
     return holders[_index];
   }
 
+  /**
+   * @dev Gets the number of token holders.
+   * @return The number of token holders.
+   */
   function getTheNumberOfHolders() public view returns(uint256) {
     return holders.length;
   }
