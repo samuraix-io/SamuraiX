5c3276636bc9849ced7995baa07128f088dc5f92
tinhnt <tinh.nt@anlab.info>
2018-05-04 10:49:36 +0700

    Update comments for test cases in RegisteredUsers.js

--- test/contracts/RegisteredUsers.js
+++ test/contracts/RegisteredUsers.js
@@ -10,28 +10,22 @@ contract('RegisteredUsers', function(accounts) {
     instance = await RegisteredUsers.deployed();
   });
 
-  it('should check user not registerd equal false', async () => {
+  it('should return false with unregistered address', async () => {
     (await instance.isUserRegistered(accounts[0])).should.be.equal(false);
   });
 
-  it('should return true when user register', async () => {
+  it('should return true with registered address', async () => {
     await instance.addRegisteredUser(accounts[1]).should.be.fulfilled;
     (await instance.isUserRegistered(accounts[1])).should.be.equal(true);
   });
 
-  it('should fulfille add user when user register', async () => {
+  it('registering new address should be fulfilled', async () => {
     await instance.addRegisteredUser(accounts[2]).should.be.fulfilled;
     (await instance.isUserRegistered(accounts[2])).should.be.equal(true);
   });
 
-  it('should reject add user when not owner call', async () => {
-    await instance.addRegisteredUser(accounts[3] , {from: accounts[2]}).should.be.rejected;
+  it('non-owner can not register new address', async () => {
+    await instance.addRegisteredUser(accounts[3], {from: accounts[2]}).should.be.rejected;
     (await instance.isUserRegistered(accounts[3])).should.be.equal(false);
-  });
-
-  it('can fulfille add user twice', async () => {
-    await instance.addRegisteredUser(accounts[4]).should.be.fulfilled;
-    await instance.addRegisteredUser(accounts[4]).should.be.fulfilled;
-    (await instance.isUserRegistered(accounts[4])).should.be.equal(true);
   });
 });
